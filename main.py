370ed176a53d2d2cc31c09ed6696b601129e2168
Moeinovic <moeinovic.z@gmail.com>
2022-07-02 19:05:46 +0430

    add error messages and video content handler

--- main.py
+++ main.py
@@ -95,8 +95,7 @@ async def download_tweet(message: types.Message):
                     await Cli.send_photo(message.chat.id, image_url, caption=caption, reply_to_message_id=message.message_id)
                 elif media_type == "video" or media_type == "animated_gif":
                     video_url = tweet_info['medias'][0]['urls'][0]["url"]
-                    if Session().head(video_url).status_code == 403:
-                        raise ContentError
+                    print(Session().head(video_url).status_code, video_url)
                     qualityies = types.InlineKeyboardMarkup()
                     for media in tweet_info['medias'][0]["urls"]:
                         resolution = media['resolution']
