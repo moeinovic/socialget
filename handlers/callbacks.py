dae06a18ccd4875e19142d6e55f95a5ba1ca5f44
Moeinovic <moeinovic.z@gmail.com>
2022-07-04 11:47:28 +0430

    try to fix callback filter

--- handlers/callbacks.py
+++ handlers/callbacks.py
@@ -80,4 +80,4 @@ async def twitter_video_dl(data: types.CallbackQuery):
         return       
 
 def register_callbacks(dp: Dispatcher):
-    dp.register_callback_query_handler(twitter_video_dl, callback.filter(lambda x: x.data.startswith("dlt_")))
+    dp.register_callback_query_handler(twitter_video_dl, callback)
--- utils/filters.py
+++ utils/filters.py
@@ -1,3 +1,3 @@
 from aiogram.utils.callback_data import CallbackData
 
-callback = CallbackData()
+callback = CallbackData("dlt_")
