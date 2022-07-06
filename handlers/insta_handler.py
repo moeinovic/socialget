0a2489f601603515cf500dd5140c28f412e1552b
Moeinovic <moeinovic.z@gmail.com>
2022-07-06 14:29:27 +0430

    insta

---
+++ handlers/insta_handler.py
@@ -0,0 +1,67 @@
+from aiogram import Bot, Dispatcher, executor, types
+from utils.config import (INSTA_SESSION, INSTAGRAM_POST_PATTERN, INSTAGRAM_PROFILE_PATTERN, INSTAGRAM_STORY_PATTERN)
+from os import  remove
+from instagrapi import Client
+from numerize import numerize as num
+from requests import Session
+from wget import download
+import traceback
+
+async def download_post(message: types.Message):
+    try:
+        insta = Client()
+        insta.login_by_sessionid(INSTA_SESSION)
+        media_info = insta.media_info(
+            insta.media_pk_from_url(
+                message.text)).dict()
+        media_type = media_info['media_type']
+        caption = media_info['caption_text']
+        media_type = media_info['media_type']
+        comment_count = media_info['comment_count']
+        like_count = media_info['like_count']
+        user_info = media_info['user']
+        full_caption = "{}\n 💬 {} 👍 {} | <a href='https://www.instagram.com/{}'>👤{}</a>".format(
+            caption,
+            num.numerize(comment_count),
+            num.numerize(like_count),
+            user_info["username"],
+            user_info['full_name'])
+        if media_type in [1, 2]:
+            if media_type == 1:
+                image_url = media_info["thumbnail_url"]
+                await message.reply_photo(image_url, caption=full_caption)
+            elif media_type == 2:
+                video_url = str(media_info["video_url"])
+                thumbnail_url = str(media_info["thumbnail_url"])
+                duration = int(media_info["video_duration"])
+                size = int(Session().head(video_url).headers["Content-Length"])
+                if size <= 20971520:
+                    message.reply_video(video_url, caption=full_caption)
+                elif size > 20971520 and size <= 52428800:
+                    status = "File size is too big please wait to download progress will be start"
+                    status_message = await message.answer(status)
+                    file = download(video_url)
+                    await message.reply_video(file, caption=full_caption)
+                    await status_message.delete()
+                    remove(file)
+                elif size >= 52428800:
+                    await message.reply("File Size is bigger than 50MB!")
+            elif media_type == 8:
+                resources = media_info["resources"]
+                media_group = types.MediaGroup()
+                with Session as s:
+                    for resource in resources:
+                        if resource["media_type"] == 1:
+                            image_url = str(resource["thumbnail_url"])
+                            media_group.append(types.InputMediaPhoto(types.InputFile(download(image_url))))
+                        elif resource["media_type"] == 2:
+                            video_url = resource["video_url"]
+                            media_group.attach_video(video_url)
+                    if len(media_group.media) == len(resources):
+                        await message.reply_media_group(media_group)
+                                
+    except Exception as e:
+        message.bot.send_message("@moeinovic", f"{traceback.format_exc()} \n {message.text}")
+
+def insta_register(dp: Dispatcher):
+    dp.register_message_handler(download_post, regexp=INSTAGRAM_POST_PATTERN)
--- main.py
+++ main.py
@@ -1,50 +1,14 @@
 from aiogram import Bot, Dispatcher, executor, types
 from handlers.callbacks import twetter_register_callbacks
 from handlers.messages import twetter_register_commands
+from handlers.insta_handler import insta_register
 from os import environ
 
 API_TOKEN = environ["API_TOKEN"]
-
 Cli = Bot(token=API_TOKEN, parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(Cli)
 
-"""@dp.message_handler(regexp=INSTAGRAM_POST_PATTERN)
-async def download_post(message: types.Message):
-    try:
-        media_info = insta.media_info(
-            insta.media_pk_from_url(
-                message.text)).dict()
-        media_type = media_info['media_type']
-        caption = media_info['caption_text']
-        media_type = media_info['media_type']
-        comment_count = media_info['comment_count']
-        like_count = media_info['like_count']
-        user_info = media_info['user']
-        full_caption = "{}\n 💬 {} 👍 {} | <a href='https://www.instagram.com/{}'>👤{}</a>".format(
-            caption,
-            num.numerize(comment_count),
-            num.numerize(like_count),
-            user_info["username"],
-            user_info['full_name'])
-        if media_type in [1, 2]:
-            if media_type == 1:
-                image_url = media_info["thumbnail_url"]
-                await Cli.send_photo(message.chat.id, image_url, caption=full_caption, reply_to_message_id=message.message_id)
-            elif media_type == 2:
-                video_url = str(media_info["video_url"])
-                size = int(head(video_url).headers["Content-Length"])
-                if size >= 20971519:
-                    status = "File size is too big please wait to download progress will be start"
-                    status_message = await message.answer(status)
-                    file = download(video_url)
-                    await Cli.send_video(message.chat.id, types.InputFile(file), caption=full_caption, reply_to_message_id=message.message_id)
-                    await status_message.delete()
-                else:
-                    await Cli.send_video(message.chat.id, video_url, caption=full_caption, reply_to_message_id=message.message_id)
-
-    except Exception as e:
-        logging.error(e)"""
-
+insta_register(dp)
 twetter_register_callbacks(dp)
 twetter_register_commands(dp)
 
