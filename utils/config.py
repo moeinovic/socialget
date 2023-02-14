f3021c24e3259c9caecc964ba5ae0f9445dead5f
Moeinovic <76170229+moeinovic@users.noreply.github.com>
2023-02-14 12:38:32 +0330

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -4,4 +4,4 @@ INSTAGRAM_POST_PATTERN = "^(https?:\/\/(?:www\.)?instagram\.com(?:\/(?!.*\.\.)(?
 INSTAGRAM_STORY_PATTERN = "^(https?:\/\/(?:www\.)?instagram\.com(?:\/(?!.*\.\.)(?!.*\.$)[^\W][\w.]{2,29})?\/stories\/([^\/?#&]+)).*$"
 INSTAGRAM_PROFILE_PATTERN = "^(https?:\\/\\/(?:www\\.)?instagram\\.com(?:\\/(?!.*\\.\\.)(?!.*\\.$)[^\\W][\\w.]{2,29})?\\/([^/?#&]+)).*$"
 TWITTER_PATTERN = "^(https?:\/\/(?:www\.)?(?:mobile\.)?twitter\.com(?:\/(?!.*\.\.)(?!.*\\.$)[^\W][\w.]{1,30})?\/status(es)?\/([\d+]{16,19}))"
-INSTA_SESSION = environ["INSTA_TOKEN"]
+INSTA_SESSION = "51209710385:pXPUqVYIefKjL5:1:AYeC5h_9C-nrp2_zUFODQcDhGuGJPDDGoY0smqz2uw"
